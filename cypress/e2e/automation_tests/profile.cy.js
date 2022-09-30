920230d879589f2acc56ba56d1f0ed922038ffec
Manish Karkera <manish.karkera@opencolleges.edu.au>
2022-09-30 20:45:30 +1000

    one test complete

--- cypress/e2e/automation_tests/profile.cy.js
+++ cypress/e2e/automation_tests/profile.cy.js
@@ -1,13 +1,15 @@
+
 describe("Login and navigate to Profile page", () => {
   it("Digital caveman approach", () => {
-    
     // https://docs.cypress.io/guides/references/error-messages#Uncaught-exceptions-from-your-application
     Cypress.on('uncaught:exception', (err, runnable) => {
       return false;
     });
 
+    cy.viewport(1250, 800);
+
     // Navigate: Login page on UAT server
-    cy.visit('https://uat-learning.opencolleges.edu.au/login/index.php');
+    cy.visit('https://uat-os.opencolleges.edu.au/login/index.php');
     // Assert: 'Log in' button in the page  
     cy.get("button[type='submit']").should("contain", "Log In");
     
@@ -18,20 +20,33 @@ describe("Login and navigate to Profile page", () => {
     // Assert: we have reached the dashboard
     cy.get("div.moduleTracker-container").should("be.visible");
 
-    // Action: Click hamburger icon on the top left 
-    cy.get("#overlayOpenMob").click();
+    // // Action: Click hamburger icon on the top left 
+    // cy.get("#overlayOpenMob").click();
     
-    // Action: Click 'My Account' button
-    cy.get("div.support-top-level-title")
-      .contains("My Account")
-      .click();
+    // // Action: Click 'My Account' button
+    // cy.get("div.support-top-level-title")
+    //   .contains("My Account")
+    //   .click();
+
+    // // Action: Click 'My Profile' button
+    // cy.get("div.os-md-menu-list")
+    // .contains("My Profile")
+    // .click();
 
     // Action: Click 'My Profile' button
-    cy.get("div.os-md-menu-list")
-    .contains("My Profile")
-    .click();
+    cy.get("#my_profile_link").parent().click();
+
     // Assert: We have successfully reached Profile page
-    cy.url().should('include', 'my-profile');
-    
-  })
+    cy.url().should("include", "my-profile");
+
+    cy.get("#detail_text-Assessment-Feedback").should("contain" ,"No Preference");
+    cy.get("#edit-button-1").click();
+    cy.get("#profile-select-Assessment\\ Feedback").should("be.visible");
+    cy.get("#profile-select-Assessment\\ Feedback").select("Audio");
+    cy.get("#save-button-1").click();
+    cy.get("#detail_text-Assessment-Feedback").should("contain" ,"Audio");
+
+  });
+
+
 })
