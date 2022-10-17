5a302ba0b08de7873ec4eca6042cd84f2ae53f01
Manish Karkera <manish.karkera@opencolleges.edu.au>
2022-10-17 15:55:01 +1100

    [DPS-1424] Replace static values

--- cypress/e2e/automation_tests/SWS-Profile.cy.js
+++ cypress/e2e/automation_tests/SWS-Profile.cy.js
@@ -1,22 +1,26 @@
+import credentials from "../../fixtures/login_credentials.json";
 
 describe("SWS-Profile", () => {
 
-  it("Grading Preference form", () => {
+  it("Grading preference form", () => {
     cy.goToProfilePage();
 
     /* Test Assessment feedback: Switch from 'No preference' to 'Audio' */
     // Assert: Current preference is set to 'No preference'
-    cy.get("#detail_text-Assessment-Feedback", { timeout: 10000 }).should("contain" ,"No Preference");
+    cy.get("#detail_text-Assessment-Feedback", { timeout: 10000 })
+      .should("contain", credentials.user_mich.assessment_feedback);
     // Action: Click edit button
     cy.get("#edit-button-1").click();
     // Assert: Form is in edit mode
     cy.get("#profile-select-Assessment-Feedback").should("be.visible");
     // Action: Select Audio
-    cy.get("#profile-select-Assessment-Feedback").select("Audio");
+    cy.get("#profile-select-Assessment-Feedback")
+      .select(credentials.user_mich.assessment_feedback_change);
     // Action: Click save 
     cy.get("#save-button-1").click();
     // Assert: Value has been changed
-    cy.get("#detail_text-Assessment-Feedback").should("contain" ,"Audio");
+    cy.get("#detail_text-Assessment-Feedback")
+      .should("contain", credentials.user_mich.assessment_feedback_change);
 
     /* Revert to original value */
     // Action: Click edit button
@@ -24,27 +28,32 @@ describe("SWS-Profile", () => {
     // Assert: Form is in edit mode
     cy.get("#profile-select-Assessment-Feedback").should("be.visible");
     // Action: Select 'No preference'
-    cy.get("#profile-select-Assessment-Feedback").select("No Preference");
+    cy.get("#profile-select-Assessment-Feedback")
+      .select(credentials.user_mich.assessment_feedback);
     // Action: Click save
     cy.get("#save-button-1").click();
     // Assert: Value has been changed
-    cy.get("#detail_text-Assessment-Feedback").should("contain" ,"No Preference");
+    cy.get("#detail_text-Assessment-Feedback")
+      .should("contain", credentials.user_mich.assessment_feedback);
 
 
-    
+
     /* Test Discussion feedback: Switch from 'No preference' to 'Audio' */
     // Assert: Current preference is set to 'No preference'
-    cy.get("#detail_text-Discussion-Feedback", { timeout: 10000 }).should("contain" ,"No Preference");
+    cy.get("#detail_text-Discussion-Feedback", { timeout: 10000 })
+      .should("contain", credentials.user_mich.discussion_feedback);
     // Action: Click edit button
     cy.get("#edit-button-1").click();
     // Assert: Form is in edit mode
     cy.get("#profile-select-Discussion-Feedback").should("be.visible");
     // Action: Select Audio
-    cy.get("#profile-select-Discussion-Feedback").select("Open to Discussion");
+    cy.get("#profile-select-Discussion-Feedback")
+      .select(credentials.user_mich.discussion_feedback_change);
     // Action: Click save
     cy.get("#save-button-1").click();
     // Assert: Value has been changed
-    cy.get("#detail_text-Discussion-Feedback").should("contain" ,"Open to Discussion");
+    cy.get("#detail_text-Discussion-Feedback")
+      .should("contain", credentials.user_mich.discussion_feedback_change);
 
     /* Revert to original value */
     // Action: Click edit button
@@ -52,10 +61,12 @@ describe("SWS-Profile", () => {
     // Assert: Form is in edit mode
     cy.get("#profile-select-Discussion-Feedback").should("be.visible");
     // Action: Select 'No preference'
-    cy.get("#profile-select-Discussion-Feedback").select("No Preference");
+    cy.get("#profile-select-Discussion-Feedback")
+      .select(credentials.user_mich.discussion_feedback);
     // Action: Click save
     cy.get("#save-button-1").click();
     // Assert: Value has been changed
-    cy.get("#detail_text-Discussion-Feedback").should("contain" ,"No Preference");
+    cy.get("#detail_text-Discussion-Feedback")
+      .should("contain", credentials.user_mich.discussion_feedback);
   });
 });
