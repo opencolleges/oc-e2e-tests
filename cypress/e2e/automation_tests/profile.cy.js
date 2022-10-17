576f4666a76b0b74eabac4a6499673c6b25f9739
Manish Karkera <manish.karkera@opencolleges.edu.au>
2022-10-18 00:56:53 +1100

    [DPS-1424] Test for study buddy settings form

--- cypress/e2e/automation_tests/profile.cy.js
+++ cypress/e2e/automation_tests/profile.cy.js
@@ -38,4 +38,39 @@ describe("Student profile page", () => {
     cy.get("#about-me-name")
       .should("contain", credentials.user_mich.name);
   });
-}) ;
+
+  it("Study Buddy from", () => {
+    cy.goToProfilePage();
+
+    /* Change study buddy settings value */
+    // Assert: Current preference
+    cy.get("#study-buddy-value")
+      .should("contain", credentials.user_mich.study_buddy);
+    // Action: Click edit button
+    cy.get("#study-buddy-edit").click();
+    // Assert: Form is in edit mode
+    cy.get("#study-buddy-select").should("be.visible");
+    // Action: Change study buddy value
+    cy.get("#study-buddy-select")
+      .select(credentials.user_mich.study_buddy_change);
+    // Action: Click save
+    cy.get("#study-buddy-save").click();
+    // Assert: Value has been changed
+    cy.get("#study-buddy-value")
+      .should("contain", credentials.user_mich.study_buddy_change);
+
+    /* Revert to original value */
+    // Action: Click edit button
+    cy.get("#study-buddy-edit").click();
+    // Assert: Form is in edit mode
+    cy.get("#study-buddy-select").should("be.visible");
+    // Action: Select original value
+    cy.get("#study-buddy-select")
+      .select(credentials.user_mich.study_buddy);
+    // Action: Click save
+    cy.get("#study-buddy-save").click();
+    // Assert: Value has been changed
+    cy.get("#study-buddy-value")
+      .should("contain", credentials.user_mich.study_buddy);
+  });
+});
