1711bcfe7694ea5c1c7a72658b3a25c77e2434c8
Manish Karkera <manish.karkera@opencolleges.edu.au>
2022-10-24 14:17:42 +1100

    [DPS-1424] Changes in ID wrt SWS-Profile tests

--- cypress/e2e/automation_tests/SWS-Profile.cy.js
+++ cypress/e2e/automation_tests/SWS-Profile.cy.js
@@ -7,66 +7,66 @@ describe("SWS-Profile", () => {
 
     /* Test Assessment feedback: Switch from 'No preference' to 'Audio' */
     // Assert: Current preference is set to 'No preference'
-    cy.get("#detail_text-Assessment-Feedback", { timeout: 10000 })
+    cy.get("#assessment-feedback-detail_text", { timeout: 10000 })
       .should("contain", credentials.user_mich.assessment_feedback);
     // Action: Click edit button
-    cy.get("#edit-button-1").click();
+    cy.get("#grading-preference-edit").click();
     // Assert: Form is in edit mode
-    cy.get("#profile-select-Assessment-Feedback").should("be.visible");
+    cy.get("#assessment-feedback-profile-select").should("be.visible");
     // Action: Select Audio
-    cy.get("#profile-select-Assessment-Feedback")
+    cy.get("#assessment-feedback-profile-select")
       .select(credentials.user_mich.assessment_feedback_change);
     // Action: Click save 
-    cy.get("#save-button-1").click();
+    cy.get("#grading-preference-save").click();
     // Assert: Value has been changed
-    cy.get("#detail_text-Assessment-Feedback")
+    cy.get("#assessment-feedback-detail_text")
       .should("contain", credentials.user_mich.assessment_feedback_change);
 
     /* Revert to original value */
     // Action: Click edit button
-    cy.get("#edit-button-1").click();
+    cy.get("#grading-preference-edit").click();
     // Assert: Form is in edit mode
-    cy.get("#profile-select-Assessment-Feedback").should("be.visible");
+    cy.get("#assessment-feedback-profile-select").should("be.visible");
     // Action: Select 'No preference'
-    cy.get("#profile-select-Assessment-Feedback")
+    cy.get("#assessment-feedback-profile-select")
       .select(credentials.user_mich.assessment_feedback);
     // Action: Click save
-    cy.get("#save-button-1").click();
+    cy.get("#grading-preference-save").click();
     // Assert: Value has been changed
-    cy.get("#detail_text-Assessment-Feedback")
+    cy.get("#assessment-feedback-detail_text")
       .should("contain", credentials.user_mich.assessment_feedback);
 
 
 
     /* Test Discussion feedback: Switch from 'No preference' to 'Audio' */
     // Assert: Current preference is set to 'No preference'
-    cy.get("#detail_text-Discussion-Feedback", { timeout: 10000 })
+    cy.get("#discussion-feedback-detail_text", { timeout: 10000 })
       .should("contain", credentials.user_mich.discussion_feedback);
     // Action: Click edit button
-    cy.get("#edit-button-1").click();
+    cy.get("#grading-preference-edit").click();
     // Assert: Form is in edit mode
-    cy.get("#profile-select-Discussion-Feedback").should("be.visible");
+    cy.get("#discussion-feedback-profile-select").should("be.visible");
     // Action: Select Audio
-    cy.get("#profile-select-Discussion-Feedback")
+    cy.get("#discussion-feedback-profile-select")
       .select(credentials.user_mich.discussion_feedback_change);
     // Action: Click save
-    cy.get("#save-button-1").click();
+    cy.get("#grading-preference-save").click();
     // Assert: Value has been changed
-    cy.get("#detail_text-Discussion-Feedback")
+    cy.get("#discussion-feedback-detail_text")
       .should("contain", credentials.user_mich.discussion_feedback_change);
 
     /* Revert to original value */
     // Action: Click edit button
-    cy.get("#edit-button-1").click();
+    cy.get("#grading-preference-edit").click();
     // Assert: Form is in edit mode
-    cy.get("#profile-select-Discussion-Feedback").should("be.visible");
+    cy.get("#discussion-feedback-profile-select").should("be.visible");
     // Action: Select 'No preference'
-    cy.get("#profile-select-Discussion-Feedback")
+    cy.get("#discussion-feedback-profile-select")
       .select(credentials.user_mich.discussion_feedback);
     // Action: Click save
-    cy.get("#save-button-1").click();
+    cy.get("#grading-preference-save").click();
     // Assert: Value has been changed
-    cy.get("#detail_text-Discussion-Feedback")
+    cy.get("#discussion-feedback-detail_text")
       .should("contain", credentials.user_mich.discussion_feedback);
   });
 });
