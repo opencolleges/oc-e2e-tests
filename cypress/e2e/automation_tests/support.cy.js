c252a72d8645cd6e877b4caecf3662cf8abdecd6
Manish Karkera <manish.karkera@opencolleges.edu.au>
2022-10-31 15:28:07 +1100

    [DPS-1427] second section of support page

--- cypress/e2e/automation_tests/support.cy.js
+++ cypress/e2e/automation_tests/support.cy.js
@@ -1,14 +1,17 @@
 import credentials from "../../fixtures/login_credentials.json";
 import supportTiles from "../../fixtures/support_page_tiles.json";
+import supportTeams from "../../fixtures/support_page_contact_teams.json";
 
 
 describe("Student support page", () => {
-  it("What do you want to know about?", () => {
+  beforeEach(() => {
     cy.login(credentials.user_mich.username, Cypress.env("password_mich"));
 
     // Action: Click support button on the navigation bar
     cy.get("#support_page_link2").click();
+  });
 
+  it("What do you want to know about?", () => {
     supportTiles.forEach(q => {
       // Action: Click one of the support tiles
       cy.get(`#${q.id}[href="${q.urlString}"]`)
@@ -25,4 +28,23 @@ describe("Student support page", () => {
       cy.get('.os-breadcrumb-last-link').click();
     })
   });
+
+  it("Still need help? Contact our support teams", () => {
+    supportTeams.forEach(q => {
+      // Action: Click one of the support teams
+      cy.get(".support-team-item")
+        .contains(q.iconText)
+        .click();
+
+      // Assert: URL contains required string
+      cy.url().should("include", q.urlString);
+
+      // Assert: Page loaded contains required title
+      cy.get(q.pageTitleDOM).should("contain", q.pageTitle);
+
+      // Action: Navigate back to support page
+      cy.go('back');
+    })
+  });
+
 });
