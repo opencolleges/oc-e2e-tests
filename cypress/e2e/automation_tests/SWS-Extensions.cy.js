f5f892b6816b66d53b57091a0f76d52bd4ab98b1
Manish Karkera <manish.karkera@opencolleges.edu.au>
2022-11-10 11:43:49 +1100

    [DPS-1468] Test for SWS-Extension

---
+++ cypress/e2e/automation_tests/SWS-Extensions.cy.js
@@ -0,0 +1,32 @@
+import credentials from "../../fixtures/login_credentials.json";
+
+describe("SWS-Extension", () => {
+  it("Course extension page", () => {
+    // https://docs.cypress.io/guides/references/error-messages#Uncaught-exceptions-from-your-application
+	Cypress.on('uncaught:exception', () => {
+		return false;
+	});
+
+	// Set the viewport
+	cy.viewport(1250, 1200);
+
+	// Navigate: Login page on UAT server
+	cy.visit(credentials.server_uat);
+	// Assert: 'Log in' button in the page
+	cy.get("button[type='submit']").should("contain", "Log In");
+
+	/* Action: Fill form and submit */
+	cy.get("input[name='username']").type(credentials.user_extension);
+	cy.get("input[name='password']").type(Cypress.env("password_mich"));
+	cy.get("button[type='submit']").click();
+
+    /* Assert: We are in the course extension page*/
+    cy.get(".content-container h3",{timeout: 10000})
+        .should("contain", "Apply for a course extension");
+
+    /* Assert: The button 'Get Started' exists */
+    cy.get("button.next-step-button")
+        .should("contain", "Get Started");
+  })
+
+});
